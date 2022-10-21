0ffbfb18888a1a9abf453284eac3b0f7a296f505
KaonWry <titan2011official@gmail.com>
2022-10-22 00:24:26 +0700

    Sistem untuk membuat barcode di pintu masuk (WIP)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 # Packages
+import datetime as dt
+from datetime import date, timedelta
 import datetime
-from datetime import timedelta
+import time
 from tkinter import *
 from barcode import EAN13
 from barcode.writer import ImageWriter
 import re
 import inquirer
 import os
+from random import seed
+from random import randint
+seed(1)
 
 
 # TUI pintu masuk 
@@ -32,11 +37,46 @@ def pintuMasuk():
         inquirer.Text
         (
             "Waktu Masuk",
-            message = "Masukan jam dan menit masuk dalam format HH:MM:SS DD/MM",
-            validate = lambda _, x: re.match('[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?\s+(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|[1][0-2])', x),
+            message = "Masukan jam dan menit masuk dalam format DD-MM-YYYY HH:MM:SS",
+            validate = lambda _, x: re.match('(0?[1-9]|[12][0-9]|3[01])-(0?[1-9]|[1][0-2])-[0-9]+\s+[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?', x),
         ),
     ]
     menuPintuMasuk = inquirer.prompt(menuPintuMasuk)
+    
+    # Tipe kendaraan
+    if(menuPintuMasuk["Tipe Kendaraan"] == "Mobil"):
+        tipeKendaraan = str("1")
+    elif(menuPintuMasuk["Tipe Kendaraan"] == "Motor"):
+        tipeKendaraan = str("2")
+    elif(menuPintuMasuk["Tipe Kendaraan"] == "Truk/Bus"):
+        tipeKendaraan = str("3")
+        
+    # Plat nomor
+    platNomor = (menuPintuMasuk["Plat Nomor"].replace(" ", "")).upper()
+    
+    # Waktu masuk
+    waktu = [0] * 2
+    waktu = menuPintuMasuk["Waktu Masuk"].split(" ")
+    pukul = [0] * 3
+    pukul = waktu[0]
+    pukul = pukul.split(":")
+    pukul = (((pukul[0]*60)+pukul[1])*60)+pukul[2]
+    tanggal = waktu[1]
+    tanggal = time.mktime(datetime.datetime.strptime(tanggal, "%d/%m/%Y").timetuple())
+    
+    print(pukul, tanggal, pukul + tanggal)
+    
+    # Membership
+    if(menuPintuMasuk["Kartu Member"]):
+        member = "Y"
+    else:
+        member = "N"
+        
+    # Angka random pengaman
+    for i in range(3):
+        random += randint(0,9)
+    
+    
     
 
 # TUI pintu keluar
