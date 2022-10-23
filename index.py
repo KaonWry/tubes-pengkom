71ac6a752ef27f89524cc8a4abc460a3e5bc7ac9
KaonWry <titan2011official@gmail.com>
2022-10-23 11:48:28 +0700

    Change project directory tree for flask

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ import datetime as dt
 from datetime import date, timedelta
 import datetime
 import time
-from barcode import EAN13
+import barcode
 from barcode.writer import ImageWriter
 import os
 from flask import Flask, request, render_template
@@ -15,12 +15,10 @@ app = Flask(__name__)
 # which URL is associated function
 @app.route('/', methods =["GET", "POST"])
 def pintuMasuk():
-    if request.method == "POST":
-       platNomor = request.form.get("tipeKendaraan")
-       return platNomor
-       
-       return "Your name is "+first_name + last_name
-    return render_template("form.html")
+    return render_template("masuk.html")
+    # if request.method == "POST":
+    #    platNomor = request.form.get("tipeKendaraan")
+    #    return platNomor
  
 if __name__=='__main__':
-   app.run()
+    app.run(debug=True)
