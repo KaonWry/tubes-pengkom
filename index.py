58842e8eed7741c37ee10908bc1252b79c3bf19b
KaonWry <titan2011official@gmail.com>
2022-10-23 15:24:13 +0700

    succesfully make the barcode appearing part, fucking finally

--- index.py
+++ index.py
@@ -4,38 +4,47 @@ from datetime import date, timedelta
 import datetime
 import sys
 import time
-import barcode
-from barcode.writer import ImageWriter
+import treepoem
 import os
 from flask import Flask, request, render_template
 
 # Flask constructor
 app = Flask(__name__)  
- 
-# A decorator used to tell the application
-# which URL is associated function
+
+# Program pintu masuk
 @app.route('/', methods =["GET", "POST"])
 def pintuMasuk():
-    tipeKendaraan = request.form.get("tipeKendaraan")
-    print (tipeKendaraan, file=sys.stderr)
-    platNomor = ((request.form.get("platNomor")).replace(" ", "")).upper()
-    print (platNomor, file=sys.stderr)
-    membership = request.form.get("membership")
-    print (membership, file=sys.stderr)
-    waktu = request.form.get("waktuMasuk")
-    waktu = waktu.split(" ")
-    # Tanggal to seconds
-    tanggal = waktu[0]
-    tanggal = int(time.mktime(datetime.datetime.strptime(tanggal, "%m/%d/%Y").timetuple()))
-    # Pukul to seconds
-    pukul = waktu[1]
-    pukul = list(map(int, pukul.split(":")))
-    if (waktu[2] == "PM"):
-        pukul[0] += 12
-    pukul = ((int(pukul[0])*60)+int(pukul[1]))*60
-    isiBarcode = str(membership) + str(pukul + tanggal) + str(tipeKendaraan) + platNomor
-    print (isiBarcode, file=sys.stderr)
-    return render_template("masuk.html", barcodeIsi = isiBarcode)
- 
+    if (request.method == "POST"):
+        tipeKendaraan = request.form.get("tipeKendaraan")
+        platNomor = ((request.form.get("platNomor")).replace(" ", "")).upper()
+        membership = request.form.get("membership")
+        waktu = request.form.get("waktuMasuk")
+        waktu = waktu.split(" ")
+        # Tanggal to seconds
+        tanggal = waktu[0]
+        tanggal = int(time.mktime(datetime.datetime.strptime(tanggal, "%m/%d/%Y").timetuple()))
+        # Pukul to seconds
+        pukul = waktu[1]
+        pukul = list(map(int, pukul.split(":")))
+        if (waktu[2] == "PM"):
+            pukul[0] += 12
+        pukul = ((int(pukul[0])*60)+int(pukul[1]))*60
+        # print (tipeKendaraan, file=sys.stderr)
+        # print (platNomor, file=sys.stderr)
+        # print (membership, file=sys.stderr)
+        # print (isiBarcode, file=sys.stderr)
+        isiBarcode = str(membership) + str(pukul + tanggal) + str(tipeKendaraan) + str(platNomor)
+        gambarBarcode = treepoem.generate_barcode(
+        barcode_type="code128",  # One of the BWIPP supported codes.
+        data = isiBarcode,
+        )
+        gambarBarcode.convert("1").save("static/img/output.png")
+        return render_template("masuk.html", barcodeIsi = isiBarcode, barcode = gambarBarcode)
+    else:
+        return render_template("masuk.html")
+
+
+# Program pintu 
+
 if __name__=='__main__':
     app.run(debug=True)
