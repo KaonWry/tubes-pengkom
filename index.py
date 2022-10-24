2556bbc51984bbc0c09bd892cb70e3b2ec3b79e0
KaonWry <titan2011official@gmail.com>
2022-10-24 13:22:47 +0700

    clean up index.py

--- index.py
+++ index.py
@@ -1,13 +1,11 @@
 # Import Modules
 from crypt import methods
-import datetime as dt
 from datetime import date, timedelta
 import datetime
 import re
 import sys
 import time
 import treepoem
-import os
 from flask import Flask, request, render_template
 
 # Flask constructor
@@ -44,10 +42,6 @@ def pintuMasuk():
         if (waktu[2] == "PM"):
             pukul[0] += 12
         pukul = ((int(pukul[0])*60)+int(pukul[1]))*60
-        # print (tipeKendaraan, file=sys.stderr)
-        # print (platNomor, file=sys.stderr)
-        # print (membership, file=sys.stderr)
-        # print (isiBarcode, file=sys.stderr)
         isiBarcode = f"{tipeKendaraan}-{platNomor}-{pukul + tanggal}-{membership}"
         gambarBarcode = treepoem.generate_barcode(
         barcode_type="code128",  # One of the BWIPP supported codes.
@@ -57,8 +51,6 @@ def pintuMasuk():
         return render_template("masuk.html", barcodeIsi = isiBarcode, barcode = gambarBarcode)
     else:
         return render_template("masuk.html")
-    
-    
     
 # Program pintu keluar
 @app.route("/k", methods=["POST", "GET"])
@@ -82,10 +74,6 @@ def pintuKeluar():
         pukul = ((int(pukul[0])*60)+int(pukul[1]))*60
         jmlhBayar = int(request.form.get("jmlhBayar"))   
         
-        # Biaya jam pertama 3000(motor), 5000(mobil), 8000(truk/bus)
-        # Biaya setiap jam 2000(motor), 3000(mobil), 5000(truk/bus)
-        # Member gratis biaya jam pertama
-        
         # Hitung lama parkir
         waktuParkir = ((pukul + tanggal) - waktuTiba)/3600
         
@@ -107,7 +95,6 @@ def pintuKeluar():
             waktuParkir = int(waktuParkir) + 1
         
         # Hitung biaya parkir
-            
         if (tipeKendaraan == "MT"):
             biayaParkir = biayaJam1 + ((waktuParkir-1) * 2000)
         elif (tipeKendaraan == "MB"):
@@ -127,36 +114,28 @@ def pintuKeluar():
             tipeKendaraan = "Mobil"
         elif (tipeKendaraan == "TR"):
             tipeKendaraan = "Truk/bus"
-
         kendaraan = f"{tipeKendaraan}, {space.join(platNomor)}"
-        
         masuk = datetime.datetime.fromtimestamp(waktuTiba).strftime('%d-%m-%Y %H:%M')
         keluar = datetime.datetime.fromtimestamp(pukul + tanggal).strftime('%d-%m-%Y %H:%M')
         lamaParkir = (pukul + tanggal) - waktuTiba
         lamaParkir = f"{int(lamaParkir/3600)}:{int((lamaParkir%3600)/60)}"
-        
         if (membership == "Y"):
             membership = "Ada"
         elif (membership == "N"):
             membership = "Tidak ada"
-        
         biayaParkir = f"Rp.{biayaParkir:,},00"
         pembayaran = f"Rp.{jmlhBayar:,},00"
         kembalian = f"Rp.{kembalian:,},00"
-        
         return render_template("keluar.html", kendaraan=kendaraan, masuk=masuk, keluar=keluar, lamaParkir=lamaParkir, membership=membership, biayaParkir=biayaParkir, pembayaran=pembayaran, kembalian=kembalian)
 
     else:
         return render_template("keluar.html")
     
     
-    
-    
 # Program buka pintu masuk
 @app.route("/bukaPintuMasuk")
 def bukaPintuMasuk():
     return render_template("masuk.html")
-
 
 # Program buka pintu keluar
 @app.route("/bukaPintuKeluar")
